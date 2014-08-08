7bb10d5b713d8a638dffa55eaf2197180ada04d2
Chris Grant <chris@instia.com>
2014-08-08 13:58:41 -0500

    Remove drawRect from bubble arrows to speed up performance during animation

--- Classes/UI/HSBubbleLeftArrow.m
+++ Classes/UI/HSBubbleLeftArrow.m
@@ -47,10 +47,10 @@
 
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    
-}
+// - (void)drawRect:(CGRect)rect
+// {
+
+// }
 
 
 @end
